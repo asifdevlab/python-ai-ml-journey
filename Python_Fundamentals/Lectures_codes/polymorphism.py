3a627f58b0c2fb82d95e206fc304ad4c24ee69af
asifdevlab <asifnawada786@gmail.com>
2026-08-31 19:55:55 +0530

    Implemented a simple program that demonstrates the function overriding

---
+++ Python_Fundamentals/Lectures_codes/polymorphism.py
@@ -0,0 +1,11 @@
+# Function Overriding
+class Employee:
+    def get_designation(self):
+        print("Designation = Employee")
+
+class Teacher(Employee):
+    def get_designation(self):
+        print("Designation = Teacher")
+
+t1 = Teacher()
+t1.get_designation()
